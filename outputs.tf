6f6071a6506e32cd6ddef6e074cff3f2808ab72e
Raj Sharma <rajesharma295@gmail.com>
2026-09-01 13:46:36 -0700

    Add Terraform outputs for Azure network

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,24 @@
-output "network_security_group_name" {
-  description = "Name of the network security group"
-  value       = azurerm_network_security_group.terraform_nsg.name
-}
 output "resource_group_name" {
-  description = "Name of the Azure resource group"
+  description = "Name of the Azure Resource Group"
   value       = azurerm_resource_group.terraform_rg.name
 }
 
 output "vnet_name" {
-  description = "Name of the virtual network"
+  description = "Name of the Virtual Network"
   value       = azurerm_virtual_network.terraform_vnet.name
 }
 
 output "vnet_address_space" {
-  description = "Address space of the virtual network"
+  description = "VNet address space"
   value       = azurerm_virtual_network.terraform_vnet.address_space
 }
 
 output "subnet_name" {
   description = "Name of the subnet"
   value       = azurerm_subnet.terraform_subnet.name
+}
+
+output "subnet_address_prefixes" {
+  description = "Subnet address prefixes"
+  value       = azurerm_subnet.terraform_subnet.address_prefixes
 }
